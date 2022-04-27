5b1fe9614a1f5d0fa945f0a0cb6e5dc37b95d860
Benjamin Schroth <info@benjamin-schroth.dev>
2022-04-27 18:17:22 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# reCAPTCHA v3 integration with Spring-Boot
+This projects is an example of how to integrate reCAPTCHA v3 with Spring-Boot using 
+the Bean Validation API.
+
+It uses a custom constraint validator with its own annotation to validate the reCAPTCHA token.
+
+Accompanies this [post]().
---
+++ src/main/java/com/example/recaptchademo/RecaptchaV3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.recaptchademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecaptchaV3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecaptchaV3DemoApplication.class, args);
+    }
+
+}
